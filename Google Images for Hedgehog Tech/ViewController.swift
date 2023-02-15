2f042e0e674f2184e886213325ad3a988d0433cb
philippzeppelin <philippzeppelin@gmail.com>
2023-02-15 21:38:39 +0300

    Initial Commit

---
+++ Google Images for Hedgehog Tech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Google Images for Hedgehog Tech
+//
+//  Created by Philipp Zeppelin on 15.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
